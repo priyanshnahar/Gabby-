ea39cb2af938050eb31138612e2d8caca4ae1dbe
Priyansh Nahar <priyanshnahar1998@gmail.com>
2023-03-06 12:12:18 +0530

    Adding files

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import { HomePage } from "scenes/homePages";
-import { LoginPage } from "scenes/loginPages";
+import LoginPage from "scenes/loginPages";
 import { ProfilePage } from "scenes/profilePages";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
+
 
 function App() {
   const mode = useSelector((state) => state.mode);
